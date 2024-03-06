0b672fdccb1af746a966962c9e5bb91c02e1c0b0
1U23CA029DHAKSHA <139437377+1U23CA029DHAKSHA@users.noreply.github.com>
2024-03-06 05:18:53 +0000

    8.j

--- Practical8.java
+++ Practical8.java
@@ -1,26 +1,54 @@
-// Animal.java
 public abstract class Animal {
-// Basic attributes and methods including abstract sound method
+protected String species;
+protected int age;
 }
-
-// Mammal.java
-public class Mammal extends Animal {
-// Additional attributes and methods
+public Animal(String species,int age){
+this.species=species;
+this.age=age;
 }
+public abstract void sound();
+@override
+public String toString{
+    return "Species:"+species+",Age:"+age;
+}
+@override
+public Boolean equals(Object obj){
 
-// Bird.java
+}
+public class Mammal extends Animal{
+}
+public void sound(){
+    System.out.println("The animal sound");
+}
 public class Bird extends Animal {
-// Additional attributes and methods
+private String featherColor;
+public Bird(String species,int age,String featherColor){
+    super(species,age);
+    this.featherColor=featherColor;
 }
-
-// Parrot.java
+public void sound(){
+System.out.println("The  bird sound");
+}
+}
 public class Parrot extends Bird {
-// Additional characteristics specific to Parrot
+private boolean canSpeak;
+public Parrot(String species,int age,String featherColor,boolean canSpeak){
+    super(species,age,featherColor);
+    this.canSpeak=canSpeak;
 }
-
-// Main.java
-public class Main {
+public void sound(){
+    System.out.println("Parrot Speaking");
+}
+else{
+    System.out.println("Parrot sound");
+}
+}
+public class Practical8{
 public static void main(String[] args) {
-// Instantiate and demonstrate object class methods
+    Mammal lion=new Mammal("Lion",5,"Grasslands");
+    Bird eagle=new Bird("Eagle",3,"Brown");
+    Parrot talkingParrot=new Parrot("Talking Parrot",2,"Green",true);
+    System.out.println("Are lion and eagle equal?"+lion.equals(eagle));
+    System.out.println("Are lion and lion equal?"+lion.equals(new Mammal("Lion",5,"Grasslands")));
 }
 }
